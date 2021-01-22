91af4c4ef29a920149798797bf4491990f63c7bb
TranTrungLe <trantrungle07051999@gmail.com>
2021-01-22 16:46:28 +0700

    finish

---
+++ Assets/Scenes/HighScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+
+    Text hightext;
+    public static int highscore = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hightext = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hightext.text = highscore.ToString();
+    }
+}
---
+++ Assets/Script/Coin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            Score.coinAmount += 1;
+            Destroy(gameObject);
+            if (Score.coinAmount > HighScore.highscore)
+            {
+                HighScore.highscore = Score.coinAmount;
+            }
+        }       
+    }
+}
